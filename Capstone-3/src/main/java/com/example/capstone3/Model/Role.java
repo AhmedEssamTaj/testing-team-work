59f4363392981075c0d3e5095d22246f59a0a5b6
AhmedEssamTaj <94526009+AhmedEssamTaj@users.noreply.github.com>
2024-12-16 19:57:47 +0300

    Add files via upload

---
+++ Capstone-3/src/main/java/com/example/capstone3/Model/Role.java
@@ -0,0 +1,19 @@
+package com.example.capstone3.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
